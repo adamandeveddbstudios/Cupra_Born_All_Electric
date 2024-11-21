1857f0cbb23f26c72b0837fcf1b3f604295cc368
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2024-11-21 18:10:38 +0530

    Add files via upload

---
+++ 320x50/main.js
@@ -0,0 +1,72 @@
+// Banner duration timer start time
+var startTime;
+
+// Timeline reference
+var tl;
+var tl1;
+var tl2;
+
+// Init tricggered by onLoad in Body tag
+function init() {
+  // Set Banner duration timer
+  startTime = new Date();
+
+  // Set Background Timeline
+  tl2 = new TimelineMax({  });
+
+  // Set Global Timeline
+  tl1 = new TimelineMax({ onComplete: endTime });
+  
+  animate();
+}
+
+function animate() {
+
+  tl1.set(["#main_content"], { autoAlpha: 1, force3D: true });
+  tl1.set(["#cta"], { force3D: false, rotation: .001 });
+  
+  // tl1.staggerTo('#text-1 span', 0.5, { autoAlpha: 1, ease: "power1.in" }, 1, 0.5);
+  tl1.to('#cta', 0.5, { autoAlpha: 1, ease: "power1.in" }, 1);
+  tl1.call(setRollover);
+
+  tl2.to('#car', 7, { scale: 1, rotation: 0.01, ease: "power1.in" }, 0);
+
+}
+
+function randomInt(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function endTime() {
+  // show total banner animation time in browser console.
+  var endTime = new Date();
+
+  console.log(
+    "Animation duration: " + (endTime - startTime) / 1000 + " seconds"
+  );
+}
+
+function randomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// CTA grow on hover
+
+function setRollover() {
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseover", default_over, false);
+  document
+    .getElementById("default_exit")
+    .addEventListener("mouseout", default_out, false);
+}
+
+function default_over(event) {
+  TweenMax.to(["#cta-roll"], 0.3, { autoAlpha: 1, ease: Power1.easeOut, delay: 0 });
+}
+
+function default_out(event) {
+  TweenMax.to(["#cta-roll"], 0.3, { autoAlpha: 0, ease: Power1.easeOut, delay: 0 });
+}
